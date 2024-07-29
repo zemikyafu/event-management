79d0ef298f804f5e8ec671dea155f1704d4fb52f
zemikyafu <zemikfcb@gmail.com>
2024-07-29 17:59:18 +0300

    Navbar component added

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import '../components/Navbar.css'
+
+function Navbar()
+{
+    return(
+        
+        <ul className="nav-bar-list">
+            <li className='nav-bar-list-item'>
+           <a className="active" href="#">Home </a>  
+            </li>
+            
+            <li className='nav-bar-list-item'>
+            <a   href="#">Events </a>  
+            </li>
+            <li className='nav-bar-list-item'>
+            <a  href="#"> Profile</a>   
+            </li>
+        </ul>
+      
+         
+    )
+}
+export default Navbar
